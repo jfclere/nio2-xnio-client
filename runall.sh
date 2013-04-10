6837bb170e25c4a2dfeb72d1b39a5c332c305250
Jean-Frederic Clere <jfclere@cluster10.mw.lab.eng.bos.redhat.com>
2013-04-10 13:32:14 -0400

    Add some more points and user cluster08

--- runall.sh
+++ runall.sh
@@ -4,12 +4,19 @@
 #
 #
 
-host=$1;
-port=$2
-n=$3;
-nReq=$4;
+#host=$1;
+#port=$2
+#n=$3;
+#nReq=$4;
 
-for i in 2000 1000 500 250 200 166 142 125 100
+#host=localhost
+host=172.17.7.254
+port=8001
+n=100;
+nReq=100000
+
+#for i in 2000 1000 500 250 200 166 142 125 100
+for i in 210 215 220 225 230 235 240 245 250 500 1000 2000
 do
 	echo ""
 	printf "sh run.sh $host $port $n $i $nReq \n";
